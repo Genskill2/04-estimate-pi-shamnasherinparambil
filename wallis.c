a25f685c6da173684c1e67643bf744ff3ec75fac
shamnasherinparambil <84335803+shamnasherinparambil@users.noreply.github.com>
2021-06-05 21:38:03 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -26,14 +26,9 @@ int main(void) {
 
 float wallis_pi(int n){
 	float pi=1.0,temp;
-	
 	for(int i=1;i<=n;i++){
-                
 		temp=4*i*i;
 		pi=(pi*(temp/(temp-1)));
-	
-	
 	}
-	
 	return (2*pi);
 }
