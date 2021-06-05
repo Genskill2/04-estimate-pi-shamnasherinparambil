22523df4fe5c57cc687cc03bc1de3a869a05fa5b
shamnasherinparambil <84335803+shamnasherinparambil@users.noreply.github.com>
2021-06-05 21:36:27 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -39,5 +39,23 @@ int main(void) {
   }
 }
 
-
+float mc_pi(int n){
+	int i=0;
+        int circle_ct=0;
+	int total_ct=0;
+	while(i<n){
+		float x = 2*frandom()-1;
+		float y = 2*frandom()-1;
+		total_ct++;
+	
+		float d=(x*x+y*y);
+		if(d<=1){
+			  circle_ct+=1;
+			}
+		
+		i++;
+	}
+	 float pi=4*(float)circle_ct/(total_ct);
+	 return (pi);
+	}
 
